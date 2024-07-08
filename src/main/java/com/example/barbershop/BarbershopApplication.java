d5a42f415d89477b0b5a33796ce8698f3be93b49
joao victor <joaovprudencio13@gmail.com>
2024-07-07 23:23:59 -0300

    configurações iniciais de dependencias

---
+++ src/main/java/com/example/barbershop/BarbershopApplication.java
@@ -0,0 +1,13 @@
+package com.example.barbershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbershopApplication.class, args);
+	}
+
+}
