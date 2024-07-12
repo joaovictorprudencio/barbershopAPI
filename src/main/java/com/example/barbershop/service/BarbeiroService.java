98fd6af9651e9e1f78be3e2ecb31ea1ec763e522
joao victor <joaovprudencio13@gmail.com>
2024-07-12 16:00:02 -0300

    função de cadastro de barbeiro

--- src/main/java/com/example/barbershop/service/BarbeiroService.java
+++ src/main/java/com/example/barbershop/service/BarbeiroService.java
@@ -1,14 +1,30 @@
 package com.example.barbershop.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.barbershop.entitys.Barbeiro;
 import com.example.barbershop.repository.BarbeiroRepository;
 
 @Service
 public class BarbeiroService {
     @Autowired
     BarbeiroRepository barbeiroRepository;
+
+    public Barbeiro CriarBarbeiro(Barbeiro barbeiro){
+        Optional<Barbeiro> barbeiroOptinal = barbeiroRepository.findByNome(barbeiro.getNome());
+         
+        if(barbeiroOptinal.isPresent()){
+            throw new ClienteException("O nome de usuário já está em uso: " + barbeiro.getNome());
+        }
+         
+        return barbeiroRepository.save(barbeiro);
+      
+    }
+
+    
 
     
 }
