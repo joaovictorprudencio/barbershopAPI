447c025111d054b671b1f281a75b575d2d776b99
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-14 13:33:09 -0300

    fixes the project

--- src/main/java/com/example/barbershop/controller/ClienteController.java
+++ src/main/java/com/example/barbershop/controller/ClienteController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/auth")
 public class ClienteController {
-     
+
     @Autowired
     ClienteService clienteService;
 
