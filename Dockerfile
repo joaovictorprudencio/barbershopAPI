ab19b7c04afc864a88134025136fb9629d345d58
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-18 11:02:01 -0300

    fixes for deploy

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ COPY src /app/src
 COPY pom.xml /app
 
 WORKDIR /app
-RUN mvn clean install
+RUN  mvn clean install -DskipTests
 
 FROM openjdk:21
 
--- src/main/java/com/example/barbershop/infraSecurity/cors/CorsConfig.java
+++ src/main/java/com/example/barbershop/infraSecurity/cors/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig  {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
