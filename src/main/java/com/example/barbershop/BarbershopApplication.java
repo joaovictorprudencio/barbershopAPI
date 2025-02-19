96f1766e3d8c2b18a2ebb46359322060762247e3
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-18 23:27:03 -0300

    adding fixes of docker and env

--- src/main/java/com/example/barbershop/BarbershopApplication.java
+++ src/main/java/com/example/barbershop/BarbershopApplication.java
@@ -1,13 +1,17 @@
 package com.example.barbershop;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import io.github.cdimascio.dotenv.Dotenv;
 @SpringBootApplication
 public class BarbershopApplication {
 
-	public static void main(String[] args) {
+	public static  void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("SPRING_DATASOURCE_URL", dotenv.get("SPRING_DATASOURCE_URL"));
+        System.setProperty("SPRING_DATASOURCE_USERNAME", dotenv.get("SPRING_DATASOURCE_USERNAME"));
+        System.setProperty("SPRING_DATASOURCE_PASSWORD", dotenv.get("SPRING_DATASOURCE_PASSWORD"));
 		SpringApplication.run(BarbershopApplication.class, args);
+
 	}
 
 }
