236b0796750dbe5eab9653c2d91edc634f4fbc70
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-02-20 16:04:48 -0300

    releasing route

--- src/main/java/com/example/barbershop/infraSecurity/SecurityConfig.java
+++ src/main/java/com/example/barbershop/infraSecurity/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/march").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/disponiveis").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/disponiveis").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .anyRequest().authenticated()
                 )
