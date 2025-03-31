0f0da285cbe95ee4f2c0bd2e7148e67a61ca78c4
joaovictorprudencio <joaovprudencio13@gmail.com>
2025-03-31 10:55:42 -0300

    configurações

--- src/main/java/com/example/barbershop/infraSecurity/cors/CorsConfig.java
+++ src/main/java/com/example/barbershop/infraSecurity/cors/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig  {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
